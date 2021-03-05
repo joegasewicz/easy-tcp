6cfb2b6cfd8954084c2a01563728f9094f3e7594
joe.gasewicz <joe.gasewicz@missiondigital.co.uk>
2021-03-05 22:03:49 +0000

    server listen, accept, read, write

--- server.c
+++ server.c
@@ -22,11 +22,13 @@
  * 
  * */
 int ET_server(int port)
-{
-
+{   
+    int listen_return = -1, client_session_fd = -1, read_return = -1, write_return = -1;
     int sockfd = ET_SOCKET_FAILURE;
     int bind_rtn = ET_BIND_FAILURE;
-    struct sockaddr_in server_addr;
+    struct sockaddr_in server_addr, client_addr;
+    int client_sock_type_len;
+    char read_buffer[20];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0)
@@ -46,12 +48,44 @@ int ET_server(int port)
     }
 
     printf(
-        "Bind success on %s:%d\n", 
+        "[EASY_TCP] Info: Listening on %s:%d\n", 
         inet_ntoa(server_addr.sin_addr),
         ntohs(server_addr.sin_port)
         );
-    printf("Closing Socket...\n");
 
+    listen_return = listen(sockfd, 10);
+    if(listen_return < 0)
+    {
+        close(sockfd);
+        ET_HANDLE_ERROR("[EASY_TCP] Listen Error:", listen_return);
+    }
+
+    client_sock_type_len = sizeof(client_addr);
+    client_session_fd = accept(sockfd, (struct sock_addr *)&client_addr, &client_sock_type_len);
+    if(client_session_fd < 0)
+    {
+        close(sockfd);
+        ET_HANDLE_ERROR("[EASY_TCP] Accept Error:", listen_return);
+    }
+
+    printf(
+        "[EASY_TCP] Info: Client request accepted on %s:%hd\n",
+        inet_ntoa(client_addr.sin_addr),
+        ntohs(client_addr.sin_port)
+        );
+
+    read_return = read(client_session_fd, read_buffer, sizeof(read_buffer));
+    printf("SERVER:: Reading from client: %s\n", read_buffer);
+    if(read_return < 0)
+    {
+        ET_HANDLE_ERROR("[EASY_TCP] Read Error:", read_return);
+    } 
+
+    write_return = write(client_session_fd, read_buffer, strlen(read_buffer)+1);
+    if(read_return < 0)
+    {
+        ET_HANDLE_ERROR("[EASY_TCP] Write Error:", write_return);
+    }
 
     close(sockfd);
 
